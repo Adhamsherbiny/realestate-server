85ffe15a8f9bb2923fe9b5b949c7a322961beab7
adhamsherbiny <adhamsherbiny.business@hotmail.com>
2024-09-16 15:33:22 +0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 import express from "express"
 import cors from "cors"
 import bcrypt from "bcrypt"
-import { databaseConnect } from "./database.js"
+import mysql from "mysql";
+ 
+const databaseConnect = mysql.createConnection({
+    host:"sql8.freesqldatabase.com",
+    user: "sql8731358",
+    password:"RFQTTVgNJh" ,
+    database:"sql8731358",
+})
+
+
+databaseConnect.connect((err)=>{
+    if(err) throw err;
+    console.log("database is ready to use")
+})
 
 const port = 5000
 const app = express()
