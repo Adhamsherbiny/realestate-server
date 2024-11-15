7d7ed9f87cb787962e75968bebf7c532ba8ecf00
adhamsherbiny <adhamsherbiny.business@hotmail.com>
2024-11-15 13:31:14 +0200

    update

--- index.js
+++ index.js
@@ -36,15 +36,9 @@ app.use(
 );
 app.use((req, res, next) => {
   res.header(
-    "Access-Control-Allow-Origin",
-    "https://real-estate-bay-theta.vercel.app"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  next();
 });
 app.use(express.json());
 
